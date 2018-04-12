e0779b25af09eae91fdb2bd846b51e3f7ae49cda
wiless <wiless.bytes@gmail.com>
2018-04-12 22:59:26 +0530

    Added configure and reset function

--- app2/main.go
+++ app2/main.go
@@ -32,6 +32,10 @@ var DRDY_PIN, CS_PIN int
 func main() {
 	InitSPI()
 	// sample code to read ADS 1247 analag samples
+	for {
+		read()
+		time.Sleep(100 * time.Millisecond)
+	}
 
 }
 
@@ -39,7 +43,7 @@ const NOP = 0xff
 
 func read() float64 {
 
-	WaitTillDRDY()
+	// WaitTillDRDY()
 
 	// ads.reset() ??
 
@@ -76,4 +80,54 @@ func WaitTillDRDY() {
 		time.Sleep(100 * time.Millisecond)
 	}
 
+}
+
+func configure() {
+	//
+	// SYS0 = 0x03
+	// WREG command = 0x40
+	// PARM=bytes to be written -1
+	// PGA = 000:1, 100:16 ,111:128 (default 1)
+	// DR Data output sampling rate SPS : (default 5 SPS), 0000:5, 1000:1000sps, >1xxx:2000 sps
+	// regbyte=[0 PGA DR]
+	// regbyte := 00001111 // PGA =1, SPS = 2k sps
+	var regbyte uint8 = 0x0F // max gain, max sps = 0111 1111 = 7F
+	data := []uint8{0x43, 0x00, regbyte}
+	var err error
+
+	err = bus.TransferAndReceiveData(data[:])
+	if err != nil {
+		log.Println("Error Reading .. ", err)
+	}
+
+	fmt.Printf("\n Received bytes %x %x %x ", data[0], data[1], data[2])
+	data = []uint8{NOP}
+	err = bus.TransferAndReceiveData(data[:])
+	if err != nil {
+		log.Println("Error Reading .. ", err)
+	}
+
+	fmt.Printf("\n Received bytes %x ", data[0])
+
+}
+
+func reset() {
+
+	data := []uint8{0x06}
+	err := bus.TransferAndReceiveData(data[:])
+	if err != nil {
+		log.Println("Error Reading .. ", err)
+	}
+
+	fmt.Printf("\n Received bytes %x ", data[0])
+
+	data[0] = 0xff
+	err = bus.TransferAndReceiveData(data[:])
+	if err != nil {
+		log.Println("Error Reading .. ", err)
+	}
+
+	fmt.Printf("\n Received bytes %x ", data[0])
+
+	time.Sleep(100 * time.Microsecond)
 }
