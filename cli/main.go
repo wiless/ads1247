d9e3a8a1435486ca90da820073439ad16be10971
ssk <ssk@5Gshell.local>
2018-11-04 13:18:10 +0530

    Added V & Current relay switching

--- cli/main.go
+++ cli/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/wiless/ads1247"
+	"github.com/wiless/gpio"
 )
 
 var DRDY_PIN, CS_PIN int
@@ -20,6 +21,9 @@ func init() {
 	flag.Parse()
 }
 
+var RelayV uint = 17
+var RelayC uint = 18
+var Vpin, Cpin gpio.Pin
 var METHOD = 1
 var interval int = 1
 
@@ -37,19 +41,114 @@ func main() {
 
 	var ch int
 
-	fmt.Printf("\n Channel to be Measured : ")
-	fmt.Scanf("%d", &ch)
+	fmt.Printf("\n MODE=1 or 2, is Infinite measurement of Voltage (Ch=0) or Current (Ch=1) \n  MODE 3 & 4 uses Voltage & Current Measurement using Relays : ")
+	fmt.Scanf("%d", &METHOD)
+	if METHOD == 1 || METHOD == 2 {
+		fmt.Printf("\n Channel to be Measured : ")
+		fmt.Scanf("%d", &ch)
 
-	fmt.Printf("\n Interval in (s) : ")
-	fmt.Scanf("%d", &interval)
-	log.Println("Starting ... channel : ", ch, " Interval : ", interval, "s")
-
-	if METHOD == 1 {
-		// sample code to read ADS 1247 analag samples
-		Method1(ch)
+		fmt.Printf("\n Interval in (s) : ")
+		fmt.Scanf("%d", &interval)
+		log.Println("Starting ... channel : ", ch, " Interval : ", interval, "s")
 	} else {
-		Method2(ch)
+		log.Println("Using GPIO %d for Voltage Relay ", RelayV)
+		log.Println("Using GPIO %d for Current Relay ", RelayC)
+		Vpin = gpio.NewOutput(RelayV, false)
+		Cpin = gpio.NewOutput(RelayC, false)
 	}
+
+	switch METHOD {
+	case 1:
+		Method1(ch)
+	case 2:
+		Method2(ch)
+	case 3:
+		MeasureVoltageCurrent(0) // type/method  1
+	case 4:
+		MeasureVoltageCurrent(1) // type/method  2
+
+	}
+
+}
+
+// Measures Voltage and Current with 1 second switch interval
+func MeasureVoltageCurrent(method int) {
+
+	if method == 0 {
+		Vpin.High()
+		Vsamples := MeasureNsamples(0, 10)
+		Vpin.Low()
+		Cpin.High()
+		Csamples := MeasureNsamples(1, 100)
+		Cpin.Low()
+
+		log.Println(Vsamples)
+		log.Println(Csamples)
+
+	}
+
+	if method == 1 {
+		Vpin.High()
+		Vsamples := MeasureNsamples2(0, 10)
+		Vpin.Low()
+		Cpin.High()
+		Csamples := MeasureNsamples2(1, 100)
+		Cpin.Low()
+
+		log.Println(Vsamples)
+		log.Println(Csamples)
+
+	}
+
+}
+
+func MeasureNsamples(ch int, Nsamples int) []float64 {
+	// var result []float64
+	result := make([]float64, Nsamples)
+	e := adc.Init(DRDY_PIN, CS_PIN)
+	if e != nil {
+		log.Panicln("Unable to Initialize ADC ")
+	}
+	adc.Initialize()
+	adc.SetChannel(ch)
+	start := time.Now()
+
+	for i := 0; i < Nsamples; i++ {
+
+		adc.WaintUntilDRDY() /// BLOCKED waiting..
+		sample := adc.ReadSample()
+		fmt.Printf("\n %v  : %v", time.Since(start), sample)
+		time.Sleep(time.Duration(interval) * time.Second)
+		result[i] = sample.Value
+
+	}
+	return result
+}
+
+func MeasureNsamples2(ch int, Nsamples int) []float64 {
+	// var result []float64
+	result := make([]float64, Nsamples)
+	var adc ads1247.ADS1247
+	e := adc.Init(DRDY_PIN, CS_PIN)
+
+	if e != nil {
+		log.Panicln("Unable to Initialize ADC ")
+	}
+	adc.Initialize()
+	adc.SetChannel(ch)
+
+	sampleCH := adc.Notify() // continuously sample ADC..
+	start := time.Now()
+	for i := 0; i < Nsamples; i++ {
+
+		s := <-sampleCH
+		// fmt.Printf("\n %v  : %v", s.TimeStamp, s.Value)
+		fmt.Printf("\n %v  : %v", time.Since(start), s.Value)
+		time.Sleep(time.Duration(interval) * time.Second)
+		result[i] = s.Value
+
+	}
+	return result
 }
 
 func Method1(ch int) {
@@ -87,7 +186,6 @@ func Method2(ch int) {
 		s := <-sampleCH
 		// fmt.Printf("\n %v  : %v", s.TimeStamp, s.Value)
 		fmt.Printf("\n %v  : %v", time.Since(start), s.Value)
-
 		time.Sleep(time.Duration(interval) * time.Second)
 	}
 
